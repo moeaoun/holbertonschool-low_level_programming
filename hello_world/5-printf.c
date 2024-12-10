0faf4f0ae1b53b63b68bc303b76e155f7fd33120
moeaoun <moe_aoun@hotmail.com>
2024-12-10 18:05:44 +0000

    5-printf.c

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - A program that print a line using the printf function
- * Return: 0 (Success)
+ * main - this is a printf fuction
+ * Return: 0
  */
 int main(void)
 {
