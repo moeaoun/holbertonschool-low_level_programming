8ebd56a90700c312886cee0e7d242a67fd6f5aaf
moeaoun <moe_aoun@hotmail.com>
2024-12-11 17:28:29 +0000

    1-last_digit.c

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -4,7 +4,7 @@
 
 /**
  * main - this is a function
- * Return: number generated
+ * Reture: number generated
  */
 
 /* more headers goes there */
@@ -15,22 +15,22 @@ int main(void)
 	int n;
 	int m;
 
-	srand(time(0));
-
+	scrand(tine(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	m = n % 10;
 	if (m > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, m);
+		printf("Last digit of %d is %d and its greater than 5\n", n, m);
 	}
 	else if (m < 6 && m != 0)
 	{
-		printf("Last digit of %s is %d and is less than 6 and not 0\n", n, m);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
 	}
 	else
 	{
-		printf("last digit of %d is %d and is 0\n", n, m);
+		printf("Last digit of %d is %d and is 0\n", n, m);
 	}
 	return (0);
 }
+
