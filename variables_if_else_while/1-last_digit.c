0e50215ae97b6f10af9a92e9cc31d7fd91fdb809
moeaoun <moe_aoun@hotmail.com>
2024-12-11 17:44:56 +0000

    1-last_digit.c

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -4,7 +4,7 @@
 
 /**
  * main - this is a function
- * Reture: number generated
+ * Return: number generated
  */
 
 /* more headers goes there */
@@ -25,7 +25,7 @@ int main(void)
 	}
 	else if (m < 6 && m != 0)
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
+		printf("Last digit of %s is %d and is less than 6 and not 0\n", n, m);
 	}
 	else
 	{
