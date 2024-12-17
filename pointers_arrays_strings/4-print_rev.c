80b7487aa93b1514fd12e1df14bf31b1492c1300
moeaoun <moe_aoun@hotmail.com>
2024-12-17 18:38:32 +0000

    4-print_rev.c

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -9,22 +9,16 @@
  */
 void print_rev(char *s)
 {
-    int len = 0;
-
-    /* Find the length of the string */
-    while (s[len] != '\0')
-    {
-        len++;
-    }
-
-    /* Print the string in reverse */
-    while (len > 0)
-    {
-        len--;
-        _putchar(s[len]);
-    }
-
-    /* Print a new line */
-    _putchar('\n');
+int len = 0;
+while (s[len] != '\0')
+{
+len++;
+}
+while (len > 0)
+{
+len--;
+_putchar(s[len]);
+}
+_putchar('\n');
 }
 
