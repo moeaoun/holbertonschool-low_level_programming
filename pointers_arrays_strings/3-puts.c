eb12f8ae51d1c1db2ff3c4c3ea1e876f425cdf2e
moeaoun <moe_aoun@hotmail.com>
2024-12-17 18:40:34 +0000

    3-puts.c

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -3,18 +3,17 @@
 /**
  * _puts - prints a string, followed by a new line
  * @str: the string to print
- *
  * Description: This function prints a given string character by character
  * using the _putchar function and then prints a new line.
  * If the string is empty, it simply prints a new line.
  */
 void _puts(char *str)
 {
-    while (*str != '\0')
-    {
-        _putchar(*str);
-        str++;
-    }
-    _putchar('\n');
+while (*str != '\0')
+{
+_putchar(*str);
+str++;
+}
+_putchar('\n');
 }
 
