63510b59110bb21954ade5351a8f7680fd0bd682
moeaoun <moe_aoun@hotmail.com>
2025-01-09 17:10:30 +0000

    0-whatsmyname.c

--- argc_argv/main.h
+++ argc_argv/main.h
@@ -1,7 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
+int _putchar(char c);   // Prototype for _putchar function
+int main(int argc, char *argv[]); // Prototype for main function
 
 #endif /* MAIN_H */
 
