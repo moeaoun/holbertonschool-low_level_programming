8cc8a0f5af0e4ae229946e1d93025f14510afc72
moeaoun <moe_aoun@hotmail.com>
2025-01-14 15:09:42 +0000

    2-print_dog.c

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -3,9 +3,9 @@
 
 /**
  * struct dog - a structure representing a dog
- * @name: the name of the dog
- * @age: the age of the dog
- * @owner: the owner of the dog
+ * @name: the name of the dog (pointer to char)
+ * @age: the age of the dog (float)
+ * @owner: the owner of the dog (pointer to char)
  */
 struct dog {
     char *name;
@@ -14,6 +14,9 @@ struct dog {
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+struct dog *new_dog(char *name, float age, char *owner);
+void free_dog(struct dog *d);
 
 #endif /* DOG_H */
 
