c87fec85e7e418b84439bd51390c24fe6d4a1ada
root <moe_aoun@hotmail.com>
2025-01-29 08:54:51 +0000

    2-add_node.c

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -7,25 +7,19 @@
  *
  * Return: the address of the new element, or NULL if it failed
  */
-list_t *add_node(list_t **head, const char *str)
+list_t *add_nodeint(listint_t **head, const int *n)
 {
-    list_t *new_node;
+    listint_t *new;
 
-    new_node = malloc(sizeof(list_t));
-    if (new_node == NULL)
+    new = malloc(sizeof(listint_t));
+    if (!new)
         return (NULL);
 
-    new_node->str = strdup(str);
-    if (new_node->str == NULL)
-    {
-        free(new_node);
-        return (NULL);
-    }
+    new->n = n;
 
-    new_node->len = strlen(str);
-    new_node->next = *head;
-    *head = new_node;
+    new->next = *head;
+    *head = new;
 
-    return (new_node);
+    return (new);
 }
 
