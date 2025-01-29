133f843c7d524df122104e390dc2f56e58584c51
root <moe_aoun@hotmail.com>
2025-01-29 09:22:32 +0000

    4-free_list.c

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -9,14 +9,12 @@
  */
 void free_list(list_t *head)
 {
-    list_t *temp;
-
-    while (head)
-    {
-        temp = head;
-        head = head->next;
-        free(temp->str);
-        free(temp);
-    }
+	while (head)
+	{
+		list_t *temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
 }
 
