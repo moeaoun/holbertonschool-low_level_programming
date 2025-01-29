4d50662182c4ad0af2a4c0aab43181a8fd8af198
root <moe_aoun@hotmail.com>
2025-01-29 09:10:51 +0000

    2-add_node.c

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -12,9 +12,10 @@
 list_t *add_node(list_t **head, const char *str)
 {
     list_t *new;
-unsigned int len = 0;
-while (str[len])
-	len++;
+    unsigned int len = 0;
+
+    while (str[len])
+        len++;
 
     new = malloc(sizeof(list_t));
     if (!new)
