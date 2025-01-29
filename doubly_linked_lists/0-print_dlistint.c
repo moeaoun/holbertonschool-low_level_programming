0624eda168a056a57898700ff25a00c2fe1d0d68
root <moe_aoun@hotmail.com>
2025-01-29 11:45:55 +0000

    0-print_dlistint.c

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -8,16 +8,16 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t num = 0;  /* Initialize the node count to zero */
+	size_t num = 0;  /* Initialize the node count to zero */
 
-    /* Traverse the list and print each node's data */
-    while (h)
-    {
-        printf("%d\n", h->n);  /* Print the value of the current node */
-        num++;  /* Increment the count for each node */
-        h = h->next;  /* Move to the next node */
-    }
+	/* Traverse the list and print each node's data */
+	while (h)
+	{
+		printf("%d\n", h->n);  /* Print the value of the current node */
+		num++;  /* Increment the count for each node */
+		h = h->next;  /* Move to the next node */
+	}
 
-    return (num);  /* Return the total number of nodes */
+	return (num);  /* Return the total number of nodes */
 }
 
