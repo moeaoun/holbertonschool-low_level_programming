7ef3de2162d3c782d2e0b240673b9f081477d1e0
root <moe_aoun@hotmail.com>
2025-01-29 11:44:55 +0000

    0-print_dlistint.c

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 
 /**
- * print_dlistint - Prints all the elements of a dlistint_t list
- * @h: Pointer to the head of the doubly linked list
+ * print_dlistint - Prints all the elements of a dlistint_t list.
+ * @h: Pointer to the head of the doubly linked list.
  *
- * Return: The number of nodes
+ * Return: The number of nodes.
  */
 size_t print_dlistint(const dlistint_t *h)
 {
