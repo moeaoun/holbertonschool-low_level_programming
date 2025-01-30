01484df840a386751801b54227e6c936135c1b52
root <moe_aoun@hotmail.com>
2025-01-30 17:13:21 +0000

    2-append_text_to_file.c

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -12,33 +12,33 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-    int fd;
-    ssize_t nwrite, len = 0;
+	int fd;
+	ssize_t nwrite, len = 0;
 
-    if (filename == NULL)
-        return (-1);
+	if (filename == NULL)
+		return (-1);
 
-    if (text_content != NULL)
-    {
-        while (text_content[len])
-            len++;
-    }
+	if (text_content != NULL)
+	{
+		while (text_content[len])
+			len++;
+	}
 
-    fd = open(filename, O_WRONLY | O_APPEND);
-    if (fd == -1)
-        return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
 
-    if (text_content != NULL)
-    {
-        nwrite = write(fd, text_content, len);
-        if (nwrite == -1)
-        {
-            close(fd);
-            return (-1);
-        }
-    }
+	if (text_content != NULL)
+	{
+		nwrite = write(fd, text_content, len);
+		if (nwrite == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
 
-    close(fd);
-    return (1);
+	close(fd);
+	return (1);
 }
 
