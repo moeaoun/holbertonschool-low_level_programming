f64a6fbec04edb48a3b56b9d3ba4d13de077a5a4
root <moe_aoun@hotmail.com>
2025-01-30 17:07:59 +0000

    1-create_file.c

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -12,33 +12,33 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-    int fd;
-    ssize_t nwrite, len = 0;
+	int fd;
+	ssize_t nwrite, len = 0;
 
-    if (filename == NULL)
-        return (-1);
+	if (filename == NULL)
+		return (-1);
 
-    if (text_content != NULL)
-    {
-        while (text_content[len])
-            len++;
-    }
+	if (text_content != NULL)
+	{
+		while (text_content[len])
+			len++;
+	}
 
-    fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-    if (fd == -1)
-        return (-1);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
 
-    if (text_content != NULL)
-    {
-        nwrite = write(fd, text_content, len);
-        if (nwrite == -1)
-        {
-            close(fd);
-            return (-1);
-        }
-    }
+	if (text_content != NULL)
+	{
+		nwrite = write(fd, text_content, len);
+		if (nwrite == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
 
-    close(fd);
-    return (1);
+	close(fd);
+	return (1);
 }
 
