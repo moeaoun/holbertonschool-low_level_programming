acda43f35d9b5d4da25a72d7c309d981eb277ffe
root <moe_aoun@hotmail.com>
2025-01-30 16:56:00 +0000

    0-read_textfile.c

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -10,42 +10,42 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd;
-    ssize_t nread, nwrite;
-    char *buffer;
+	int fd;
+	ssize_t nread, nwrite;
+	char *buffer;
 
-    if (filename == NULL)
-        return (0);
+	if (filename == NULL)
+		return (0);
 
-    buffer = malloc(sizeof(char) * letters);
-    if (buffer == NULL)
-        return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
 
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-    {
-        free(buffer);
-        return (0);
-    }
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		free(buffer);
+		return (0);
+	}
 
-    nread = read(fd, buffer, letters);
-    if (nread == -1)
-    {
-        free(buffer);
-        close(fd);
-        return (0);
-    }
+	nread = read(fd, buffer, letters);
+	if (nread == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
 
-    nwrite = write(STDOUT_FILENO, buffer, nread);
-    if (nwrite == -1 || nwrite != nread)
-    {
-        free(buffer);
-        close(fd);
-        return (0);
-    }
+	nwrite = write(STDOUT_FILENO, buffer, nread);
+	if (nwrite == -1 || nwrite != nread)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
 
-    free(buffer);
-    close(fd);
-    return (nwrite);
+	free(buffer);
+	close(fd);
+	return (nwrite);
 }
 
