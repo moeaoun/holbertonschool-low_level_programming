c6d4e7453d36d92b23a4004063c116865d443b4b
root <moe_aoun@hotmail.com>
2025-01-30 17:17:31 +0000

    3-cp.c

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -16,12 +16,14 @@ int main(int argc, char *argv[])
 	int fd_from, fd_to, nread, nwrite;
 	char buffer[1024];
 
+	/* Error if the number of arguments is incorrect */
 	if (argc != 3)
 	{
-		dprintf(2, "Usage: %s file_from file_to\n", argv[0]);
+		dprintf(2, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
+	/* Open the source file for reading */
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 	{
@@ -29,6 +31,7 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
+	/* Open the destination file for writing (create it if it doesn't exist) */
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
@@ -36,6 +39,7 @@ int main(int argc, char *argv[])
 		exit(99);
 	}
 
+	/* Read from the source file and write to the destination file */
 	while ((nread = read(fd_from, buffer, 1024)) > 0)
 	{
 		nwrite = write(fd_to, buffer, nread);
@@ -46,12 +50,14 @@ int main(int argc, char *argv[])
 		}
 	}
 
+	/* Check if reading from the source file failed */
 	if (nread == -1)
 	{
 		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 
+	/* Close the file descriptors */
 	if (close(fd_from) == -1)
 	{
 		dprintf(2, "Error: Can't close fd %d\n", fd_from);
